99131dd75cdeb9498a575b2ea5e8a52f258d7f76
Sinjin Cooper <coopersinjin@gmail.com>
2020-04-21 14:54:32 -0700

    first add

---
+++ CollatzTest.py
@@ -0,0 +1,10 @@
+def collatz(n):
+    if n == 1:
+        return 0
+    elif (n % 2) == 0:
+        return 1 + collatz(n/2)
+    else:
+        return 1 + collatz((3*n) + 1)
+
+num = input("Input a number: ")
+print(collatz(int(num)))
